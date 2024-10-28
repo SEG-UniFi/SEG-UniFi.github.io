4f1d87a076bc57b45083969ad42372726877f324
Whitney <whitneynsegbe@gmail.com>
2024-10-28 00:46:07 -0400

    worked on main landing page

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
+import '../styles/components/about.css'
+import '../styles/components/header.css'
+import '../styles/components/index.css'
+import '../styles/components/overview.css'
+import '../styles/components/survey.css'
 // utils.css has to be the last css file
 import '../styles/utils.css'
